44f3946ce67e0fd08d604f64be5e14c0a3d61412
wtomershafir <tomersha@wix.com>
2022-06-19 16:14:31 +0300

    feat: add client creation log

--- packages/greyhound-js/src/greyhound_client_factory.js
+++ packages/greyhound-js/src/greyhound_client_factory.js
@@ -2,6 +2,7 @@ const grpc = require('@grpc/grpc-js');
 const services = require('../proto/com/wixpress/dst/greyhound/sidecar/api/v1/greyhoundsidecar_grpc_pb');
   
 function getClient(host, port) {
+  console.log(`Creating client of Greyhound running at ${host}:${port}`)
   return new services.GreyhoundSidecarClient(`${host}:${port}`, grpc.credentials.createInsecure());
 }
 
