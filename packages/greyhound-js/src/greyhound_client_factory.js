31d361e92cc4bc850f5e35ede5da7397372b16af
wtomershafir <tomersha@wix.com>
2022-06-19 14:53:00 +0300

    fix: remove redundant ref

--- packages/greyhound-js/src/greyhound_client_factory.js
+++ packages/greyhound-js/src/greyhound_client_factory.js
@@ -2,7 +2,7 @@ const grpc = require('@grpc/grpc-js');
 const services = require('../proto/com/wixpress/dst/greyhound/sidecar/api/v1/greyhoundsidecar_grpc_pb');
   
 function getClient(host, port) {
-  return client = new services.GreyhoundSidecarClient(`${host}:${port}`, grpc.credentials.createInsecure());
+  return new services.GreyhoundSidecarClient(`${host}:${port}`, grpc.credentials.createInsecure());
 }
 
 module.exports = {getClient};
