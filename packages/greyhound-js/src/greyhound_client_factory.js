5565c2b6143700a60b7c8e5eb3794903399e1842
tomershafir <tomer.shafir8@gmail.com>
2022-07-23 17:40:19 +0300

    chore: add default gh config + single upfront fetch

--- packages/greyhound-js/src/greyhound_client_factory.js
+++ packages/greyhound-js/src/greyhound_client_factory.js
@@ -1,11 +1,14 @@
 const grpc = require('@grpc/grpc-js'),
   services = require('../proto/com/wixpress/dst/greyhound/sidecar/api/v1/greyhoundsidecar_grpc_pb');
-  
+
+const host = process.env.GREYHOUNDHOST ? process.env.GREYHOUNDHOST : "localhost",
+  port = process.env.GREYHOUNDPORT ? process.env.GREYHOUNDPORT : "3735";
+
 let client = null;
 function getClient() {
   if (null === client) {
-    console.log(`creating client of Greyhound process which is running at ${process.env.GREYHOUNDHOST}:${process.env.GREYHOUNDPORT}`)
-    client = new services.GreyhoundSidecarClient(`${process.env.GREYHOUNDHOST}:${process.env.GREYHOUNDPORT}`, grpc.credentials.createInsecure());
+    console.log(`creating client of Greyhound process which is running at ${host}:${port}`)
+    client = new services.GreyhoundSidecarClient(`${host}:${port}`, grpc.credentials.createInsecure());
   }
   return client;
 }
